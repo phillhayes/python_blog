bc5bd005c66717d74c33e937a0089cb89d559b99
phillhayes <phillip@kloodle.com>
2017-02-25 21:02:58 +0000

    make migrations

--- blog/settings/local.py
+++ blog/settings/local.py
@@ -6,7 +6,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'phill',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
--- blog/settings/production.py
+++ blog/settings/production.py
@@ -6,9 +6,5 @@ DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite')
-}
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] =  dj_database_url.config()
 
