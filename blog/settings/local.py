0da7308f1eeb731fdd7afe5e85ac28b032722daa
phillhayes <phillip@kloodle.com>
2017-02-25 21:42:10 +0000

    make migrations

--- blog/settings/local.py
+++ blog/settings/local.py
@@ -4,13 +4,3 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'phill',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
