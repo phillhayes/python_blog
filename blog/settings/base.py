f30b2d75a102fab71980469e111a2076fbe42159
phillhayes <phillip@kloodle.com>
2017-02-26 09:53:48 +0000

    add

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-DATABASES = {'default': dj_database_url.config(default='postgres://grlioeawbrlzmg:89699375894665079a05a693cb9668c2f1fb4c6d966aa5b2f93f3903b6e66b65@ec2-23-21-76-49.compute-1.amazonaws.com:5432/d49m5uihliqf7e')}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
