497ae8f91706c57483e3f0aa0867662a6792fc82
phillhayes <phillip@kloodle.com>
2017-05-02 11:30:57 +0100

    db chaneg

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -6,14 +6,5 @@ DEBUG = True
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd49m5uihliqf7e',
-        'USER': 'grlioeawbrlzmg',
-        'PASSWORD': '89699375894665079a05a693cb9668c2f1fb4c6d966aa5b2f93f3903b6e66b65',
-        'HOST': 'postgres://grlioeawbrlzmg:89699375894665079a05a693cb9668c2f1fb4c6d966aa5b2f93f3903b6e66b65@ec2-23-21-76-49.compute-1.amazonaws.com:5432/d49m5uihliqf7e',
-        'PORT': '',
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
 
